6a454e8d1583b23d0d3dbd572fe5795b0867bbc6
Tanya Selvog <tanyaselvog@gmail.com>
2020-09-04 12:32:30 -0700

    added style.css, edited app.js to include lib opts

--- src/app.ts
+++ src/app.ts
@@ -13,12 +13,14 @@ const fetchData = () : void => {
         getPokemon(i)
     }
 }
+
+
 const getPokemon = async (id: number): Promise<void> => {
-    const data; Response = await fetch ('`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const pokemon: any = await data.json()
     const pokemonType: string = pokemon.types
-    .map((poke: any) => poke.type.name)
-    .join(", ")
+      .map((poke: any) => poke.type.name)
+      .join(", ") 
 
 const transformedPokemon = {
     id: pokemon.id,
@@ -27,5 +29,20 @@ const transformedPokemon = {
     type: pokemonType,
 }
 
-showPokemon(transformPokemon)
+showPokemon(transformedPokemon)
 }
+
+const showPokemon = (pokemon: IPokemon): void => {
+    let output: string = `
+          <div class="card">
+              <span class="card--id">#${pokemon.id}</span>
+              <img class="card--image" src=${pokemon.image} alt=${pokemon.name} />
+              <h1 class="card--name">${pokemon.name}</h1>
+              <span class="card--details">${pokemon.type}</span>
+          </div>
+      `
+    container.innerHTML += output
+  }
+  
+  fetchData()
+  
